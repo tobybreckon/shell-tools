7d0a66c8f9197a37eecd1e4043de987b40392cb4
tobybreckon <toby.breckon@durham.ac.uk>
2023-04-28 11:13:25 +0100

    add nvtop + nano to baseline

--- ubuntu-package-install.sh
+++ ubuntu-package-install.sh
@@ -143,7 +143,7 @@ case $1 in
 
    # everything else ....
    
-   sudo apt install vlc exfat-fuse exfat-utils cmake-qt-gui v4l-utils v4l-conf
+   sudo apt install vlc exfat-fuse exfat-utils cmake-qt-gui v4l-utils v4l-conf nvtop htop nano
 
    ##########
     ;;
