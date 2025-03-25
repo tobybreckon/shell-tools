79794d1d090c5a504442932843b5f07ae905463f
tobybreckon <toby.breckon@durham.ac.uk>
2025-03-25 00:01:05 +0000

    make chrome a repo base install/entry

--- ubuntu-package-install.sh
+++ ubuntu-package-install.sh
@@ -26,8 +26,11 @@ case $1 in
 
     # google chrome browser
 
-    sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install ./google-chrome-stable_current_amd64.deb
+    wget https://dl-ssl.google.com/linux/linux_signing_key.pub
+    sudo apt-key add linux_signing_key.pub
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+    sudo apt-get install google-chrome-stable
     ;;
 
   zoom)
